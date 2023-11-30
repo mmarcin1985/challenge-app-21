a8f1fdbcd2d1b00450e0a02334d2d028c4cd4178
mmarc <mmarc@LAPTOP-FNOQCG0U>
2023-11-30 21:10:28 +0100

    wer2

--- challenge app21/challenge app21/Program.cs
+++ challenge app21/challenge app21/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*liczny całkowite
+int myAge = 120;
+int newAge = myAge + 5;
+//Console.WriteLine(newAge);
+int myVar = int.MinValue;
+uint myVar2 = uint.MaxValue;
+
+//liczby zmienno przecinkowe
+ulong myVar3 = ulong.MaxValue;
+float myNumber = float.MaxValue;
+double myNumer = double.MaxValue;
+
+// zmienne tekstowe
+string name = "Marcin";
+string surname = "Brzoska";
+string results = name + " " + surname  +  myNumber;
+//Console.Write(results);
+char myVar5 = 'a';
+//var result2 = name.ToArray();
+
+//zmienna logiczna
+bool isActive = true;
+isActive  = false;
+var isValid = 5 > 6;
+Console.WriteLine(isValid); */
+
+var number1 = 7;
+var number2 = 6;
+
+if (number1 > number2)
+{
+Console.WriteLine("jestem w lini 31");
+}
+else
+{
+Console.WriteLine("Jestem w lini 36");
+}
